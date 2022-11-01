08cf586bd9abc78a698617f17fe0510ee2d12598
Thiago Grillo <thiago.grillo@cartorio24h.local>
2022-11-01 17:25:18 -0300

    Mais exemplos

--- example2_refatorado.js
+++ example2_refatorado.js
@@ -3,14 +3,12 @@ let scrape = async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto('http://books.toscrape.com/')
-  const result = await page.evaluate(_ => 
-    Array.from(
-      document.querySelectorAll('section > div > ol > li img'))
-              .map(books => books.getAttribute('alt'))
+  const result = await page.$$eval('li img', titles =>
+    titles.map(titles => titles.getAttribute('alt'))
   )
   browser.close()
   return result
-}
+};
 scrape().then((value) => {
   console.log(value)
 })
